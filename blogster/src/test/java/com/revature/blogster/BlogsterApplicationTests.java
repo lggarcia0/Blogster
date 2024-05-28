4a41dca7a5fb488ce944c959bb08c9684e75e68e
briandb3 <75955003+briandb3@users.noreply.github.com>
2024-05-28 14:38:42 -0400

    Spring initializer file with Spring Security, Spring w/ Kafka in addition to normal options

---
+++ blogster/src/test/java/com/revature/blogster/BlogsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.blogster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
