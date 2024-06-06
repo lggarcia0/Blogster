e4d9a68d8cbb3e8dc804b745e724dc5f74a84f41
lggarcia0 <luisgarcia@alumni.unc.edu>
2024-06-06 13:09:14 -0400

    modified the blog model names to be shorter and added an endpoint to create a new blog

--- blogster/src/main/java/com/revature/exceptions/InvalidUser.java
+++
@@ -1,7 +0,0 @@
-package com.revature.exceptions;
-
-public class InvalidUser extends RuntimeException{
-    public InvalidUser(String message) {
-        super(message);
-    }
-}
--- blogster/src/main/java/com/revature/repositories/BlogRepository.java
+++ blogster/src/main/java/com/revature/repositories/BlogRepository.java
@@ -3,7 +3,9 @@ package com.revature.repositories;
 import com.revature.models.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface BlogRepository extends JpaRepository<Blog, Integer> {
+    Optional<Blog> findByName(String blogName);
 }
